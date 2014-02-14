728825c17fd464c3d847436ef79ebdc903440405
mikesobczak <msobczak@yahoo.com>
2014-02-14 12:06:40 -0500

    Initial commit of this project.

---
+++ HelloWorld/src/com/msobczak/test/TestServlet.java
@@ -0,0 +1,36 @@
+package com.msobczak.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("Hello, world!");
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		doGet(req, resp);
+	}
+	
+	
+
+}
